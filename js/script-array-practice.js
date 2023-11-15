9c36a6ea1d1f45e14e4fb8cb5de946291b8b46a1
Melody G <melodyflash@gmail.com>
2023-11-14 18:53:25 -0800

    init

---
+++ js/script-array-practice.js
@@ -0,0 +1,140 @@
+//STEP 1
+// let faveMovies = ['Pulp Fiction', 'Us', 'Shutter Island', 'Nightmare on Elm Street', 'Free Solo']
+// console.log(faveMovies[1])
+
+//STEP 2
+// let movies = new Array(5)
+// movies[0] = 'Pulp Fiction'
+// movies[1] = 'Us'
+// movies[2] = 'Shutter Island'
+// movies[3] = 'Nightmare on Elm Street'
+// movies[4] = 'Free Solo'
+
+// console.log(movies[0])
+
+//STEP 3
+// let movies = new Array(5)
+// movies[0] = 'Pulp Fiction'
+// movies[1] = 'Us'
+// movies[2] = 'Shutter Island'
+// movies[3] = 'Nightmare on Elm Street'
+// movies[4] = 'Free Solo'
+
+// movies[2] = 'Fine Lines'
+// movies[5] = 'Shutter Island'
+// console.log(movies.length)
+
+//STEP 4
+// let movies1 = []
+// movies1[0] = 'Pulp Fiction'
+// movies1[1] = 'Us'
+// movies1[2] = 'Shutter Island'
+// movies1[3] = 'Nightmare on Elm Street'
+// movies1[4] = 'Free Solo'
+
+// delete movies1[0]
+// console.log(movies1)
+
+//STEP 5
+// let movies1 = []
+// movies1[0] = 'Pulp Fiction'
+// movies1[1] = 'Us'
+// movies1[2] = 'Shutter Island'
+// movies1[3] = 'Nightmare on Elm Street'
+// movies1[4] = 'Free Solo'
+// movies1[5] = 'Fine Lines'
+// movies1[6] = 'Aristocats'
+
+// for (let i in movies1) {
+// 	if (movies1.hasOwnProperty(i)) {
+// 		console.log(movies1[i])
+// 	}
+// }
+
+
+
+//STEP 6
+// let movies1 = []
+// movies1[0] = 'Pulp Fiction'
+// movies1[1] = 'Us'
+// movies1[2] = 'Shutter Island'
+// movies1[3] = 'Nightmare on Elm Street'
+// movies1[4] = 'Free Solo'
+// movies1[5] = 'Fine Lines'
+// movies1[6] = 'Aristocats'
+
+// for (let i of movies1) {
+// 	console.log(i)
+// }
+
+//STEP 7
+
+// let movies1 = []
+// movies1[0] = 'Pulp Fiction'
+// movies1[1] = 'Us'
+// movies1[2] = 'Shutter Island'
+// movies1[3] = 'Nightmare on Elm Street'
+// movies1[4] = 'Free Solo'
+// movies1[5] = 'Fine Lines'
+// movies1[6] = 'Aristocats'
+
+// for (let i of movies1.sort()) {
+// 	console.log(i)
+// }
+
+
+//STEP 8
+let movies1 = []
+movies1[0] = 'Pulp Fiction'
+movies1[1] = 'Us'
+movies1[2] = 'Shutter Island'
+movies1[3] = 'Nightmare on Elm Street'
+movies1[4] = 'Free Solo'
+movies1[5] = 'Fine Lines'
+movies1[6] = 'Aristocats'
+
+let leastFavMovies = []
+leastFavMovies[0] = 'Lazarus effect'
+leastFavMovies[1] = 'Critters'
+leastFavMovies[2] = 'Puppet master'
+
+console.log('Movies I like:')
+console.log(' ')
+console.log(movies1[0])
+console.log(movies1[1])
+console.log(movies1[2])
+console.log('...')
+console.log(' ')
+console.log('Movies I regret watching')
+console.log(' ')
+console.log(leastFavMovies[0])
+console.log(leastFavMovies[1])
+console.log(leastFavMovies[2])
+console.log('...')
+
+//STEP 9
+
+
+//STEP 10
+
+
+//STEP 11
+
+
+//STEP 12
+
+
+//STEP 13
+
+
+//STEP 14
+
+
+//STEP 15
+
+
+//STEP 16
+
+
+//STEP 17
+
