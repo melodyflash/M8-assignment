c5db41c1d365513205173b8a30fa691013e620ff
Melody G <melodyflash@gmail.com>
2023-11-21 09:39:32 -0800

    in progress but late so turning some of this in

--- js/script-array-practice.js
+++ js/script-array-practice.js
@@ -84,53 +84,150 @@
 
 
 //STEP 8
-let movies1 = []
-movies1[0] = 'Pulp Fiction'
-movies1[1] = 'Us'
-movies1[2] = 'Shutter Island'
-movies1[3] = 'Nightmare on Elm Street'
-movies1[4] = 'Free Solo'
-movies1[5] = 'Fine Lines'
-movies1[6] = 'Aristocats'
+// let movies1 = []
+// movies1[0] = 'Pulp Fiction'
+// movies1[1] = 'Us'
+// movies1[2] = 'Shutter Island'
+// movies1[3] = 'Nightmare on Elm Street'
+// movies1[4] = 'Free Solo'
+// movies1[5] = 'Fine Lines'
+// movies1[6] = 'Aristocats'
 
-let leastFavMovies = []
-leastFavMovies[0] = 'Lazarus effect'
-leastFavMovies[1] = 'Critters'
-leastFavMovies[2] = 'Puppet master'
+// let leastFavMovies = []
+// leastFavMovies[0] = 'Lazarus effect'
+// leastFavMovies[1] = 'Critters'
+// leastFavMovies[2] = 'Puppet master'
 
-console.log('Movies I like:')
-console.log(' ')
-console.log(movies1[0])
-console.log(movies1[1])
-console.log(movies1[2])
-console.log('...')
-console.log(' ')
-console.log('Movies I regret watching')
-console.log(' ')
-console.log(leastFavMovies[0])
-console.log(leastFavMovies[1])
-console.log(leastFavMovies[2])
-console.log('...')
+// console.log('Movies I like:')
+// console.log(' ')
+// console.log(movies1[0])
+// console.log(movies1[1])
+// console.log(movies1[2])
+// console.log('...')
+// console.log(' ')
+// console.log('Movies I regret watching')
+// console.log(' ')
+// console.log(leastFavMovies[0])
+// console.log(leastFavMovies[1])
+// console.log(leastFavMovies[2])
+// console.log('...')
 
 //STEP 9
+// let movies1 = []
+// movies1[0] = 'Pulp Fiction'
+// movies1[1] = 'Us'
+// movies1[2] = 'Shutter Island'
+// movies1[3] = 'Nightmare on Elm Street'
+// movies1[4] = 'Free Solo'
+// movies1[5] = 'Fine Lines'
+// movies1[6] = 'Aristocats'
 
+// let leastFavMovies = []
+// leastFavMovies[0] = 'Lazarus effect'
+// leastFavMovies[1] = 'Critters'
+// leastFavMovies[2] = 'Puppet master'
+
+// let movies = movies1.concat(leastFavMovies)
+
+// console.log(movies.reverse())
 
 //STEP 10
+// let movies1 = []
+// movies1[0] = 'Pulp Fiction'
+// movies1[1] = 'Us'
+// movies1[2] = 'Shutter Island'
+// movies1[3] = 'Nightmare on Elm Street'
+// movies1[4] = 'Free Solo'
+// movies1[5] = 'Fine Lines'
+// movies1[6] = 'Aristocats'
 
+// let leastFavMovies = []
+// leastFavMovies[0] = 'Lazarus effect'
+// leastFavMovies[1] = 'Critters'
+// leastFavMovies[2] = 'Puppet master'
+
+// let movies = movies1.concat(leastFavMovies)
+
+// console.log(movies.slice(-1))
 
 //STEP 11
+// let movies1 = []
+// movies1[0] = 'Pulp Fiction'
+// movies1[1] = 'Us'
+// movies1[2] = 'Shutter Island'
+// movies1[3] = 'Nightmare on Elm Street'
+// movies1[4] = 'Free Solo'
+// movies1[5] = 'Fine Lines'
+// movies1[6] = 'Aristocats'
 
+// let leastFavMovies = []
+// leastFavMovies[0] = 'Lazarus effect'
+// leastFavMovies[1] = 'Critters'
+// leastFavMovies[2] = 'Puppet master'
+
+// let movies = movies1.concat(leastFavMovies)
+
+// console.log(movies[0])
 
 //STEP 12
+
+// console.log(`${movies[7]} is index ${movies.indexOf('Lazarus effect')}`)
+// console.log(`${movies[8]} is index ${movies.indexOf('Critters')}`)
+// console.log(`${movies[9]} is index ${movies.indexOf('Puppet master')}`)
+// delete movies[7]
+// delete movies[8]
+// delete movies[9]
+// movies[7] = 'Get out'
+// movies[8] = 'Nueve reinas'
+// movies[9] = 'Cronocrimenes'
+// console.log(movies)
 
 
 //STEP 13
 
+// let movies = [
+//     ['Fine Lines', 1],
+//     ['Us', 2],
+//     ['Free Solo', 3],
+//     ['Shutter Island', 4],
+//     ['Nightmare on Elm Street', 5]
+// ]
+
+// let namesOnly = movies.filter((item) => {
+//     return typeof item !== "number"
+// })
+
+// console.log(namesOnly)
 
 //STEP 14
 
+// let employees = []
+
+// employees[0] = 'ZAK'
+// employees[1] = 'JESSICA'
+// employees[2] = 'MARK'
+// employees[3] = 'FRED'
+// employees[4] = 'SALLY'
+
+// let showEmployee = (passedArray) => { 
+//     console.log('Employees:')
+//     console.log(' ')
+//     passedArray.forEach((item) => {
+//         console.log(item)
+//     })
+// }
+
+// showEmployee(employees)
 
 //STEP 15
+
+// let filterValues = [58, '', 'abcd', true, null, false, 0]
+
+// let filteredValues = filterValues.filter((item) => {
+//     return typeof item !== "boolean"
+// })
+
+// console.log(filteredValues)
 
 
 //STEP 16
--- js/script-ems.js
+++ js/script-ems.js
@@ -1,12 +1,20 @@
 // CREATE AN ARRAY OF EMPLOYEES
-
+let employees = [
+    ['0001231', 'Melody G', '1231', 'melodyflash@gmail.com', 'Executive'],
+    ['0001232', 'Kit T', '1232', 'melodyflash@gmail.com', 'Administrative'],
+    ['0001233', 'Lucy P', '1233', 'melodyflash@gmail.com', 'Sales'],
+    ['0001234', 'Amelia P', '1234', 'melodyflash@gmail.com', 'Sales'],
+    ['0001235', 'Barbara P', '1235', 'melodyflash@gmail.com', 'Marketing']
+]
 
 // CHECK TO SEE IF STORAGE OBJECT EXISTS WHEN THE PAGE LOADS
 // IF DOES, RETURN STORAGE OBJECT INTO ARRAY INSTEAD OF POPULATED ARRAY
 
 
 // GET DOM ELEMENTS
-
+let form     = document.querySelector('#addForm')
+let empTable    = document.querySelector('#empTable')
+let empCount    = document.querySelector('#empCount')
 
 // BUILD THE EMPLOYEES TABLE WHEN THE PAGE LOADS
 
@@ -14,9 +22,13 @@
 // ADD EMPLOYEE
 form.addEventListener('submit', (e) => {
     // PREVENT FORM SUBMISSION
-
+    e.preventDefault()
     // GET THE VALUES FROM THE TEXT BOXES
-
+    let empID       = document.querySelector('#id').value;
+    let empName     = document.querySelector('#name').value;
+    let empExt      = document.querySelector('#extension').value;
+    let empEmail    = document.querySelector('#email').value;
+    let empDept     = document.querySelector('#department').value;
     // ADD THE NEW EMPLOYEE TO A NEW ARRAY OBJECT
 
     // PUSH THE NEW ARRAY TO THE *EXISTING* EMPLOYEES ARRAY
@@ -24,9 +36,9 @@ form.addEventListener('submit', (e) => {
     // BUILD THE GRID
 
     // RESET THE FORM
-
+    document.querySelector('#addForm').reset();
     // SET FOCUS BACK TO THE ID TEXT BOX
-
+    document.querySelector('#id').focus();
 });
 
 // DELETE EMPLOYEE
